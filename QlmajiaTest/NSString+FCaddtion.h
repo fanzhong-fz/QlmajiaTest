a57a483289b6eabc2f20f9253ed45d036cc288ff
fanzhong <fanzhong@qianlu.com>
2018-11-26 17:31:28 +0800

    second commit

---
+++ QlmajiaTest/NSString+FCaddtion.h
@@ -0,0 +1,13 @@
+//
+//  NSString+FCaddtion.h
+//  QlmajiaTest
+//
+//  Created by fanzhong on 18/4/16.
+//  Copyright © 2018年 fanzhong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (FCaddtion)
+
+@end
